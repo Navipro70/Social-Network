61954d74ec903e8eb74b91bb6686743edf7ccfa8
Dmitry <dmitry@neattech.io>
2020-03-30 17:33:09 +0300

    Refactoring dialogs

--- src/Components/Dialogs/Dialogs.js
+++ src/Components/Dialogs/Dialogs.js
@@ -1,11 +1,41 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import classes from "./Dialogs.module.css";
+
+const DialogId = props => {
+  return (
+    <NavLink
+      to={`/dialogs/${props.id}`}
+      activeClassName={classes.active}
+      className={classes.name}
+    >
+      {props.name}
+    </NavLink>
+  );
+};
+
+const Message = props => {
+  return <div className={classes.message}>{props.mess}</div>;
+};
 
 const Dialogs = () => {
   return (
-    <div>
-      <div>dialog window</div>
-      <div>Main dialog</div>
+    <div className={classes.gridContent}>
+      <div className={classes.names}>
+        <div>Direct messages</div>
+        <DialogId name="Dmitry" id="1" />
+        <DialogId name="Sasha" id="2" />
+        <DialogId name="Kostya" id="3" />
+        <DialogId name="Diana" id="4" />
+        <DialogId name="Andrew" id="5" />
+        <DialogId name="Vadya" id="6" />
+      </div>
+
+      <div className={classes.dialog}>
+        <Message mess="Hi" />
+        <Message mess="How are you?" />
+        <Message mess="I'm ok thank you" />
+      </div>
     </div>
   );
 };
